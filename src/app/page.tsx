f7876fec18057eef43574456008f1815b05c99a7
tanwarabhinandan <tanwar.abhinandan@gmail.com>
2024-06-11 00:19:14 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-primary-500">
-        Wel Come to Digigyan E-Book Store
-      </h1>
+      <Banner />
     </>
   );
 }
