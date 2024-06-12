48f6128306e9d3d4140588f5ee236f9bc3c8f61e
tanwarabhinandan <tanwar.abhinandan@gmail.com>
2024-06-13 03:29:17 +0530

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto py-4">
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold text-4xl mb-4  text-indigo-500">About Us</h1>
+      </div>
+      <p>
+        Welcome to DIGIGYAN E-book Store, your ultimate destination for digital
+        reading pleasure! At DIGIGYAN, we are passionate about books and
+        committed to providing a seamless and enriching reading experience for
+        book lovers everywhere.
+      </p>
+      <div className="flex justify-center items-center">
+        <h2 className="font-bold text-2xl  text-indigo-500">Our Story</h2>
+      </div>
+      <p>
+        DIGIGYAN was founded with the vision of making a vast and diverse
+        collection of ebooks accessible to readers worldwide. In a world where
+        technology and convenience are key, we recognized the need for a
+        dedicated platform that offers not just ebooks, but an immersive reading
+        experience. Our journey began in 2024, and since then, we have been
+        dedicated to building a community of readers and authors who share our
+        love for literature.
+      </p>
+      <div className="flex justify-center items-center">
+        <h2 className="font-bold text-2xl text-indigo-500">Our Mission</h2>
+      </div>
+      <p>
+        Our mission is to ignite a passion for reading by offering a diverse
+        range of ebooks that cater to all tastes and preferences. Whether you
+        are a fan of timeless classics, contemporary fiction, non-fiction, or
+        niche genres, we strive to provide a comprehensive library that meets
+        your needs. We are dedicated to fostering a love for reading by making
+        ebooks easily accessible and affordable.
+      </p>
+      <div className="flex justify-center items-center">
+        <h2 className="font-bold text-2xl text-indigo-500">What We Offer</h2>
+      </div>
+      <div>
+        <ul>
+          <li>Extensive Library</li>
+          <p>
+            With thousands of titles across various genres, we ensure that you
+            have access to both the latest bestsellers and hidden gems.
+          </p>
+          <li>User-Friendly Experience</li>
+          <p>
+            Our intuitive platform is designed to make browsing, purchasing, and
+            reading ebooks as easy as possible.
+          </p>
+          <li>Exclusive Deals </li>
+          <p>
+            Enjoy special discounts, offers, and promotions regularly to make
+            your reading journey more affordable.
+          </p>
+          <li>Author Spotlight</li>
+          <p>
+            We believe in supporting authors. Our Author Spotlight section
+            features emerging writers and provides them a platform to reach a
+            wider audience.
+          </p>
+          <li>Personalized Recommendations</li>
+          <p>
+            Discover new favorites with our personalized recommendation system
+            tailored to your reading preferences.
+          </p>
+          <li>Community Engagement</li>
+          <p>
+            Join our vibrant community of readers through book clubs, discussion
+            forums, and author events.
+          </p>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
+
+// Why Choose Us?
+// At [Your Online Ebook Store], we understand that reading is more than just a hobby—it's a way of life. We are committed to providing:
+
+// Quality Content: We meticulously curate our collection to ensure that you have access to high-quality, engaging, and informative ebooks.
+// Customer Support: Our dedicated customer support team is always ready to assist you with any inquiries or issues you may encounter.
+// Secure Transactions: Your privacy and security are our top priorities. We ensure that all transactions are safe and secure.
+// Accessibility: Enjoy reading on any device, anytime, anywhere. Our ebooks are compatible with a wide range of e-readers, tablets, and smartphones.
+// Thank you for choosing [Your Online Ebook Store] as your trusted source for digital reading. We look forward to being a part of your literary journey and helping you discover new worlds, one ebook at a time.
+
+// Happy Reading!
+
+// Team DIGIGYAN
